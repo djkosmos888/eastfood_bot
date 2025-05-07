b53bd444252c4d8425aaacf183dad071eda23f28
djkosmos888 <japansib@gmail.com>
2025-05-07 15:55:33 +0300

    Delete client.py

--- client.py
+++
@@ -1,7 +0,0 @@
-from aiogram import types, Dispatcher
-
-async def start_handler(message: types.Message):
-    await message.answer("Привет! Пожалуйста, укажи юридическое лицо, контакт и адрес доставки, чтобы оформить заказ.")
-
-def register_handlers(dp: Dispatcher):
-    dp.register_message_handler(start_handler, commands=["start"])
